d6ccdd36e483373f907034905854c9ae977ef41e
Deepak Yadav <deepakry937@gmail.com>
2024-11-22 21:17:48 +0530

    Second method

--- Python programs/1_Factorial.py
+++ Python programs/1_Factorial.py
@@ -1,5 +1,5 @@
 # A simple Python program to calculate the factorial of a given number:
-
+    
 def factorial(n):
     if n == 0:
         return 1
@@ -9,3 +9,9 @@ def factorial(n):
 num = int(input("Enter a number: "))
 
 print("Factorial of", num, "is", factorial(num))
+
+#  This Program has some minor flaws 
+
+# The code does not handle cases where the user inputs a negative number.
+
+# If the user enters a non-integer or invalid input (like a string or float), the program will throw an error
