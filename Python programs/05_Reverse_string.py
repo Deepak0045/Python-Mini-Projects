2f65b30cddb206477743991c509f6bc1f72a6753
Deepak Yadav <deepakry937@gmail.com>
2024-11-27 13:53:00 +0530

    Reverse a string

---
+++ Python programs/05_Reverse_string.py
@@ -0,0 +1,22 @@
+print('Method 1')
+
+def reverse_string(word):
+    return word[::-1]
+
+user_input = input('Enter a word : ')
+print(f'The reversed string is : ', reverse_string(user_input))
+
+print('')
+
+print('Method 2')
+
+def reverse(word):
+    return word[::-1]
+
+while True:
+    user_input = input('Enter a word : ')
+    if user_input.isalpha():
+        print(f'The reversed string is : ', reverse(user_input))
+        break
+    else:
+        print("Invalid input. Please enter letters only (no numbers or special characters).")
