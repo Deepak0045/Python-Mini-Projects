9bf9f4bd0abba21170995a5a0cacaf2b4cb851e1
Deepak Yadav <deepakry937@gmail.com>
2024-12-01 15:22:40 +0530

    Method 2

--- Python programs/06_Pelindrome_Check.py
+++ Python programs/06_Pelindrome_Check.py
@@ -9,3 +9,17 @@ if user_input == is_palindrome:
     print(f"{user_input} is a palindrome.")
 else:
     print(f"{user_input} is not a palindrome.")
+
+
+print('Method 2')
+# Get input from the user
+user_input = input('Enter a word: ')
+
+# Reverse the user input
+reversed_input = ''.join(reversed(user_input))
+
+# Check if the original input is the same as the reversed string
+if user_input == reversed_input:
+    print(f"{user_input} is a palindrome.")
+else:
+    print(f"{user_input} is not a palindrome.")
